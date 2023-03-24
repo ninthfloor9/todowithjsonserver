23d90682eff564318718579fae3ea35dbb00383a
root <ninthflooor@gmail.com>
2023-03-24 16:14:00 +0900

    Add axios with recoil selector

--- src/app2.jsx
+++ src/app2.jsx
@@ -3,38 +3,62 @@ import React from "react";
 import { useRecoilValue } from "recoil";
 import { selector } from "recoil";
 
-//fetch with global state
-const apiUrl = "https://jsonplaceholder.typicode.com/posts";
+//fetch
+const getApi = async () => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    return response.data || [];
+  } catch (error) {
+    console.log("Error!", error.response.status);
+    return [];
+  }
+};
 
+const postApi = async (e) => {
+  e.preventDefault();
+  const title = e.target.title.value;
+  const body = e.target.body.value;
+  try {
+    const response = await axios.post(
+      "https://jsonplaceholder.typicode.com/posts",
+      { title, body }
+    );
+    console.log(response); // 응답 코드 확인
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//fetch selector
 const fetchApi = selector({
   key: "FetchApi",
   get: async () => {
-    try {
-      const response = await axios.get(apiUrl);
-      return response.data || [];
-    } catch (error) {
-      console.log("Error! : \n${error}");
-      return [];
-    }
+    const data = await getApi();
+    return data;
+  },
+  set: async ({ set, get }, value) => {
+    const data = await postApi(value);
+    set(fetchApi, [...get(fetchApi), data]);
   },
 });
 
-//local state , input
-
-// post 요청 시 state 변경에 대한 표시가 이루어져야 한다
+//rendering
 export const App2 = () => {
   const dummyData = useRecoilValue(fetchApi);
-  console.log("state", fetchApi);
   return (
     <div className="App2">
+      <form onSubmit={postApi}>
+        <input name="title" />
+        <input name="body" />
+        <input type="submit" value="추가" />
+      </form>
       <h1>App2 data</h1>
-      {dummyData.map((product) => (
-        <div key={product.id}>
-          <h3>{product.title}</h3>
-          <p>{product.body}</p>
-        </div>
-      ))}
-      ;
+      <h3>{dummyData[dummyData.length - 1].title}</h3>
+      <p>{dummyData[dummyData.length - 1].body}</p>
     </div>
   );
 };
