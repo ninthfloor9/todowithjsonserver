57402413df3b421dee919516a4d5e170f044f030
root <ninthflooor@gmail.com>
2023-03-29 23:54:33 +0900

    Create fetch.js

---
+++ src/fetch.js
@@ -0,0 +1,27 @@
+const apiUrl = "https://jsonplaceholder.typicode.com/users/1/posts";
+
+//1. get posts fetch
+
+import axios from "axios";
+
+export const getPosts = async () => {
+  try {
+    const response = await axios.get(apiUrl);
+    return response.data || [];
+  } catch (error) {
+    console.log("Error가 났어요", error.response.status);
+    return [];
+  }
+};
+
+//2. post posts fetch
+
+export const postPosts = async () => {
+  try {
+    const response = await axios.post(apiUrl, {});
+    return response || [];
+  } catch (error) {
+    console.log("Error가 났어요", error.response.status);
+    return [];
+  }
+};
