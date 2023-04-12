115fd18c8de8c6302f1ce941a887b82a32bb37fe
ninth hoom <ninthflooor@gmail.com>
2023-04-12 18:26:36 +0900

    Add react-query

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RecoilRoot } from "recoil";
 import { App3 } from "./app";
+import { QueryClient, QueryClientProvider } from "react-query";
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <React.Suspense>
-        <App3 />
-      </React.Suspense>
-    </RecoilRoot>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <RecoilRoot>
+        <React.Suspense>
+          <App3 />
+        </React.Suspense>
+      </RecoilRoot>
+    </React.StrictMode>
+  </QueryClientProvider>
 );
